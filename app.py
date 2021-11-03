7d6a82bd4a96ad878cf5f30456b3526a5f6af322
Marco Naylon <marconaylon@gmail.com>
2021-11-03 16:45:29 -0600

    update acct nbr

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
 from test_pipeline.test_pipeline_stack import TestPipelineStack
 from pipeline import Pipeline
 
-PIPELINE_ENV = cdk.Environment(account="222222222222", region="us-east-1")
+PIPELINE_ENV = cdk.Environment(account="225342792054", region="us-east-1")
 
 app = cdk.App()
 
