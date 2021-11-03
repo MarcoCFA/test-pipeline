f32ee2220170584fcb8bbeed9cedac1986dc7325
Marco Naylon <marconaylon@gmail.com>
2021-11-03 17:54:22 -0600

    chore: commit save point

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,12 @@
 
 from aws_cdk import Stack
-from aws_cdk import aws_codepipeline as pipelines
+from aws_cdk import pipelines
 from aws_cdk import aws_codebuild as codebuild
 
 
 class Pipeline(Stack):
-    def __init__(self, scope, id_: str, **kwargs):
-        super().__init__(scope, id_, **kwargs)
+    def __init__(self, scope, id, *, description=None, env=None, stackName=None, tags=None, synthesizer=None, terminationProtection=None, analyticsReporting=None):
+        super().__init__(scope, id, description=description, env=env, stackName=stackName, tags=tags, synthesizer=synthesizer, terminationProtection=terminationProtection, analyticsReporting=analyticsReporting)
 
     # Source
     github_repo = pipelines.CodePipelineSource.connection("MarcoCFA/test-pipeline", "master", connection_arn="arn:aws:codestar-connections:us-east-1:225342792054:connection/0d952b85-4245-42bc-8619-a03128f24aa9")
