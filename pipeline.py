8da388f860a52ddb20d29d91b955605f8636c049
Marco Naylon <marconaylon@gmail.com>
2021-11-03 18:22:46 -0600

    Used example from docs

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
 
 from aws_cdk import Stack
 from aws_cdk import pipelines
+from aws_cdk import aws_codepipeline
 from aws_cdk import aws_codebuild as codebuild
 
 
@@ -8,10 +9,15 @@ class Pipeline(Stack):
     def __init__(self, scope, id, *, description=None, env=None, stackName=None, tags=None, synthesizer=None, terminationProtection=None, analyticsReporting=None):
         super().__init__(scope, id, description=description, env=env, stackName=stackName, tags=tags, synthesizer=synthesizer, terminationProtection=terminationProtection, analyticsReporting=analyticsReporting)
 
-    # Source
-    github_repo = pipelines.CodePipelineSource.connection("MarcoCFA/test-pipeline", "master", connection_arn="arn:aws:codestar-connections:us-east-1:225342792054:connection/0d952b85-4245-42bc-8619-a03128f24aa9")
+        # Source
+        github_repo = pipelines.CodePipelineSource.connection("MarcoCFA/test-pipeline", "master",
+                                                              connection_arn="arn:aws:codestar-connections:us-east-1:225342792054:connection/0d952b85-4245-42bc-8619-a03128f24aa9")
 
-    # Pipeline
-    pipeline = pipelines.CodePipeline("GitHubPipeline",cross_account_keys=False,synth=pipelines.ShellStep("Synth",input=github_repo,commands=["npm ci", "npm run build", "npx cdk synth"]))
-
-
+        github_pipeline = pipelines.CodePipeline(self, "Pipeline",
+            synth=pipelines.ShellStep("Synth",
+                input=github_repo
+                ),
+                commands=["npm ci", "npm run build", "npx cdk synth"
+                ]
+            )
+        )
