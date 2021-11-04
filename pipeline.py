7d617a4226bc5ecf5cb863dcdfd5fed406c608fe
Marco Naylon <marconaylon@gmail.com>
2021-11-03 18:05:28 -0600

    removed self from CodePipeline() method

--- pipeline.py
+++ pipeline.py
@@ -12,6 +12,6 @@ class Pipeline(Stack):
     github_repo = pipelines.CodePipelineSource.connection("MarcoCFA/test-pipeline", "master", connection_arn="arn:aws:codestar-connections:us-east-1:225342792054:connection/0d952b85-4245-42bc-8619-a03128f24aa9")
 
     # Pipeline
-    pipeline = pipelines.CodePipeline(self,"GitHubPipeline",cross_account_keys=False,synth=pipelines.ShellStep("Synth",input=github_repo,commands=["npm ci", "npm run build", "npx cdk synth"]))
+    pipeline = pipelines.CodePipeline("GitHubPipeline",cross_account_keys=False,synth=pipelines.ShellStep("Synth",input=github_repo,commands=["npm ci", "npm run build", "npx cdk synth"]))
 
 
