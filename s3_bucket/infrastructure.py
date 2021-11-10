7e309cd0c7723700cc617a4394ade6d0d3b0db08
Marco Naylon <marconaylon@gmail.com>
2021-11-10 10:57:22 -0700

    updated s3

--- s3_bucket/infrastructure.py
+++ s3_bucket/infrastructure.py
@@ -5,8 +5,10 @@ from aws_cdk import aws_s3 as s3
 class TestBucket(Construct):
     def __int__(self, scope: Construct, id_: str, *, bucket_name: str, **kwargs):
         super().__int__()(scope, id, **kwargs)
+        self.removal_policy = cdk.RemovalPolicy.DESTROY
 
-        self.bucket = s3.Bucket(self, bucket_name=bucket_name, removal_policy=cdk.RemovalPolicy)
+        self.bucket = s3.Bucket(self, "test-bucket", bucket_name=bucket_name, removal_policy=self.removal_policy)
+
 
 
 
