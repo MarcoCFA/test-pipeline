0d671ae5bf3899e2db30e2506e020b2e492d8b94
Marco Naylon <marconaylon@gmail.com>
2021-11-10 10:27:55 -0700

    updated method

--- s3_bucket/infrastructure.py
+++ s3_bucket/infrastructure.py
@@ -3,14 +3,12 @@ from constructs import Construct
 from aws_cdk import aws_s3 as s3
 
 class TestBucket(Construct):
-    def __int__(self, scope: Construct, id_: str, *, test_bucket_name: str, **kwargs):
+    def __int__(self, scope: Construct, id_: str, *, bucket_name: str, **kwargs):
         super().__int__()(scope, id, **kwargs)
 
-        self.bucket = None
-        self.bucket_name = test_bucket_name
-
-    def create_bucket(self):
         self.bucket = s3.Bucket(self, bucket_name=self.bucket_name, removal_policy=cdk.RemovalPolicy)
+        self.bucket_name = bucket_name
+
 
 
 
